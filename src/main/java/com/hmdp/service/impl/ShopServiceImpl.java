6a1fa55921dcf45dbdcf5bcc2ecc463d39335261
KuChanMaster <liushuaibzinga@outlook.com>
2025-02-13 21:30:04 +0800

    redis登录验证功能

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -18,49 +18,46 @@ import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
-@Resource
-private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private ShopMapper shopMapper;
 
     @Override
     public Result queryById(Long id) {
         //查询redis,若存在则转化对象后返回
-        String key=CACHE_SHOP_KEY+id;
-        String shopJson=stringRedisTemplate.opsForValue().get(key);
-        if(StringUtils.isBlank(shopJson)){
-            Shop shop= JSONUtil.toBean(shopJson,Shop.class);
+        String key = CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //这⾥判断的时shopJson是否真的有值，不包括空值
+        if (StringUtils.isNotBlank(shopJson)) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-        Shop shop=shopMapper.selectById(id);
-        if(shop==null){
+
+        if (shopJson != null) {
+            return Result.fail("店铺信息不存在");
+        }
+
+        Shop shop = shopMapper.selectById(id);
+        if (shop == null) {
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_SHOP_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在");
         }
-        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
     }
 
     @Override
     @Transactional//保障原子性
     public Result update(Shop shop) {
-        Long id= shop.getId();
-        if (id==null){
+        Long id = shop.getId();
+        if (id == null) {
             return Result.fail("店铺不存在");
         }
         //先更新数据库后删除缓存
         shopMapper.updateById(shop);
-        stringRedisTemplate.delete(CACHE_SHOP_KEY+id);
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
         return Result.ok();
-
     }
 }
