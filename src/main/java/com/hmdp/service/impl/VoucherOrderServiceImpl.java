bcd5fd12c90cc808b6acf80660b88a45b7027937
KuChanMaster <liushuaibzinga@outlook.com>
2025-03-21 22:05:24 +0800

    * 当线程1获取锁后，由于业务阻塞，线程1的锁超时释放了，  * 这时候线程2趁虚而入拿到了锁，然后此时线程1业务完成了，  * 然后把线程2刚刚获取的锁给释放了，这时候线程3又趁虚而入拿到了锁，  * 这就导致又出现了超卖问题！

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,10 +9,12 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +32,9 @@ import java.time.LocalDateTime;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
 
     @Resource
     private ISeckillVoucherService seckillVoucherService;
@@ -55,48 +60,63 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
          * 因为此时用户下单在并发状态下可以重复下单造成了黄牛的一个问题，所以需要一人一单
          * */
         Long userId = UserHolder.getUser().getId();
-        synchronized (userId.toString().intern()) {
-            //通过代理对象，进行方法调用，避免事务的失败
-            IVoucherOrderService proxy = (IVoucherOrderService)AopContext.currentProxy();
-            return proxy.createVoucherOrder(voucherId);
-        }
-    }
+        /**
+         * synchronized (userId.toString().intern()) {
+         *             //通过代理对象，进行方法调用，避免事务的失败
+         *             IVoucherOrderService proxy = (IVoucherOrderService)AopContext.currentProxy();
+         *             return proxy.createVoucherOrder(voucherId);
+         *         }
+         */
 
-    @Transactional
-    public Result createVoucherOrder(Long voucherId) {
-        //一人一单
-        Long userId = UserHolder.getUser().getId();
-        LambdaQueryWrapper<VoucherOrder> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper
-                .eq(VoucherOrder::getUserId, voucherId)
-                .eq(VoucherOrder::getVoucherId, voucherId);
-        int count = this.count(queryWrapper);
-
-        if (count > 0) {
-            return Result.fail("您已购买过此商品");
+        SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
+        if (!lock.tryLock(5)) {
+            return Result.fail("服务器繁忙");
         }
-        //扣除库存
-        boolean success = seckillVoucherService
-                .update()
-                .setSql("stock=stock-1")
-                .eq("voucher_id", voucherId)
-                .gt("stock", 0)//乐观锁解决超卖问题（CAS法）
-                .update();
-        if (!success) {
-            return Result.fail("被抢光了 >_< ");
-        }
-        //创建订单
         try {
-            Class<?> clazz = Class.forName("com.hmdp.entity.VoucherOrder");
-            Constructor<?> constructor = clazz.getConstructor();
-            VoucherOrder vocherOrder = (VoucherOrder) constructor.newInstance();
-            long orderId = redisIdWorker.createId("order");
-            vocherOrder.setId(orderId).setUserId(userId).setVoucherId(voucherId);
-            this.save(vocherOrder);
-            return Result.ok(orderId);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            //拿到代理对象，进⾏⽅法调⽤，避免事务失效。
+            IVoucherOrderService proxy = (IVoucherOrderService)
+                    AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }finally {
+            lock.unlock();
+        }
         }
 
+        @Transactional
+        public Result createVoucherOrder (Long voucherId){
+            //一人一单
+            Long userId = UserHolder.getUser().getId();
+            LambdaQueryWrapper<VoucherOrder> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper
+                    .eq(VoucherOrder::getUserId, voucherId)
+                    .eq(VoucherOrder::getVoucherId, voucherId);
+            int count = this.count(queryWrapper);
+
+            if (count > 0) {
+                return Result.fail("您已购买过此商品");
+            }
+            //扣除库存
+            boolean success = seckillVoucherService
+                    .update()
+                    .setSql("stock=stock-1")
+                    .eq("voucher_id", voucherId)
+                    .gt("stock", 0)//乐观锁解决超卖问题（CAS法）
+                    .update();
+            if (!success) {
+                return Result.fail("被抢光了 >_< ");
+            }
+            //创建订单
+            try {
+                Class<?> clazz = Class.forName("com.hmdp.entity.VoucherOrder");
+                Constructor<?> constructor = clazz.getConstructor();
+                VoucherOrder vocherOrder = (VoucherOrder) constructor.newInstance();
+                long orderId = redisIdWorker.createId("order");
+                vocherOrder.setId(orderId).setUserId(userId).setVoucherId(voucherId);
+                this.save(vocherOrder);
+                return Result.ok(orderId);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
-}
--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,34 +1,58 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
 public class SimpleRedisLock implements ILock {
-    private String name;
+    private String name;//锁的名称
     private StringRedisTemplate stringRedisTemplate;
 
-    public static final String KEY_PREFIX = "lock";
+    public static final String KEY_PREFIX = "lock";//锁的前缀
+
+/**
+ * 当线程1获取锁后，由于业务阻塞，线程1的锁超时释放了，
+ * 这时候线程2趁虚而入拿到了锁，然后此时线程1业务完成了，
+ * 然后把线程2刚刚获取的锁给释放了，这时候线程3又趁虚而入拿到了锁，
+ * 这就导致又出现了超卖问题！
+ * */
+
+    /**
+     * ID前缀
+     */
+    public static final String ID_PREFIX = UUID.randomUUID().toString(true);
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    /**
+     * @param timeoutSec 超时时间
+     */
     @Override
-    public boolean tryLock(long timeoutSet) {
+    public boolean tryLock(long timeoutSec) {
 
+        String threadId = ID_PREFIX + Thread.currentThread().getId() + " ";
         Boolean success = stringRedisTemplate
                 .opsForValue()
-                .setIfAbsent(KEY_PREFIX + name,String.valueOf(Thread.currentThread().getId()));
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
 
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX+name);
+        //判断锁的线程标识是否与当前线程一致
+        String currentThreadFlag = ID_PREFIX + Thread.currentThread().getId();
+        String redisThreadFlag = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if (redisThreadFlag.equals(currentThreadFlag)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
+
     }
 }
